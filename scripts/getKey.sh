f0ff174a714ab5d8ad4a705503a9896e5c6e90fe
karanotts <45759232+karanotts@users.noreply.github.com>
2019-02-18 08:23:26 +0000

    Update getKey.sh

--- scripts/getKey.sh
+++ scripts/getKey.sh
@@ -1,4 +1,7 @@
-storagekey=$(az storage account keys list \
+#!/bin/bash
+key=$(az storage account keys list \
     --resource-group $(terraformstoragerg) \
     --account-name $(terraformstorageaccount) \
     --query "[0].value" | tr -d '"')
+
+echo "##vso[task.setvariable variable=storagekey]$key"
