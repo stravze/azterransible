c08d8fcb66a30d62c228368607d76286a2d1737e
karanotts <45759232+karanotts@users.noreply.github.com>
2019-02-18 15:00:37 +0000

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "************* execute terraform init"
+echo "TERRAFORM INIT"
 cd ../terraform 
 
 terraform init \
-    -backend-config= # awaiting input
+    -backend-config="storage_account_name=$1" \
+    -backend-config="container_name=$2" \
+    -backend-config="key=$3/terraform.tfstate" 
+    
