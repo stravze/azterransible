eac1b0c763612dece841b8d83dd1405b4579d9b2
karanotts <45759232+karanotts@users.noreply.github.com>
2019-02-18 10:34:15 +0000

    Update createContainer.sh

--- scripts/createContainer.sh
+++ scripts/createContainer.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-az storage container create --name terraform --account-name $(terraformstorageaccount)
+STORAGEACCT=$(az storage account create \
+    --resource-group $(terraformstoragerg) \
+    --name "rdoperftesttf$RANDOM" \
+    --location uksouth \
+    --sku Standard_LRS \
+    --query "name" | tr -d '"')
+    
+echo "##vso[task.setvariable variable=storagekey;isOutput=true]$STORAGEACCT"
