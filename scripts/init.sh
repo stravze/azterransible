cfefde9f041e72f109a54545dbc451b8db2578a8
karanotts <45759232+karanotts@users.noreply.github.com>
2019-02-20 10:16:17 +0000

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,12 +2,9 @@
 echo "TERRAFORM INIT"
 cd ../terraform 
 
-echo $1
-echo $2
-
-# terraform init \
-#     -backend-config="storage_account_name=$1" \
-#     -backend-config="container_name=terraform" \
-#     -backend-config="key=terraform.tfstate" \
-#     -backend-config="access_key=$2"
+terraform init \
+    -backend-config="storage_account_name=$1" \
+    -backend-config="container_name=terraform" \
+    -backend-config="key=terraform.tfstate" \
+    -backend-config="access_key=$2"
     
