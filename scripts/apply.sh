dce383a37c97cc2ecc86549825b582ae6259d92b
karanotts <45759232+karanotts@users.noreply.github.com>
2019-02-21 21:31:59 +0000

    Update apply.sh

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -8,5 +8,8 @@ export ARM_SUBSCRIPTION_ID=$3
 export ARM_TENANT_ID=$4
 export ARM_ACCESS_KEY=$5
 export SSH_PUB_KEY=$6
+export SSH_KEY=$7
 
-terraform apply -auto-approve -var "ssh_key=$6"
+terraform apply -auto-approve \
+  -var "ssh_key=$6" \
+  -var "private_key=$7"
