33d2e762f4d4a0c9d36a6f33329946eb499368e9
karanotts <45759232+karanotts@users.noreply.github.com>
2019-02-21 13:46:31 +0000

    Update plan.sh

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -9,4 +9,10 @@ export ARM_TENANT_ID=$4
 export ARM_ACCESS_KEY=$5
 export SSH_PUB_KEY=$6
 
-terraform plan -var ssh_key=$6
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+# terraform plan -var ssh_key=$6
