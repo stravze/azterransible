2065834d41a456e113a68058f9734a21079e2b33
karanotts <45759232+karanotts@users.noreply.github.com>
2019-02-21 20:39:29 +0000

    Create apply.sh

---
+++ scripts/apply.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "TERRAFORM APPLY"
+cd ../terraform 
+
+export ARM_CLIENT_ID=$1
+export ARM_CLIENT_SECRET=$2
+export ARM_SUBSCRIPTION_ID=$3
+export ARM_TENANT_ID=$4
+export ARM_ACCESS_KEY=$5
+export SSH_PUB_KEY=$6
+
+terraform apply -auto-approve -var "ssh_key=$6"
