f16cd3bde536667e8c0529349b1c83f94e5af02e
karanotts <45759232+karanotts@users.noreply.github.com>
2019-02-21 13:18:46 +0000

    Update plan.sh

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -2,4 +2,11 @@
 echo "TERRAFORM PLAN"
 cd ../terraform 
 
-terraform plan -var ssh_key=$1
+export ARM_CLIENT_ID=$1
+export ARM_CLIENT_SECRET=$2
+export ARM_SUBSCRIPTION_ID=$3
+export ARM_TENANT_ID=$4
+export ARM_ACCESS_KEY=$5
+export SSH_PUB_KEY=$6
+
+terraform plan -var ssh_key=$6
