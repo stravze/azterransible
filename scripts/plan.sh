5c1dcdc63960e259ff9dfbdbe72e39eecb4d4149
karanotts <45759232+karanotts@users.noreply.github.com>
2019-02-21 10:11:42 +0000

    Update plan.sh

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -1,2 +1,9 @@
-cd ../terraform/
-terraform plan -var-file="..\terraform\terraform.tfvars" 
+#!/bin/bash
+echo "TERRAFORM INIT"
+cd ../terraform 
+
+terraform plan \
+    -backend-config="storage_account_name=$1" \
+    -backend-config="container_name=terraform" \
+    -backend-config="key=terraform.tfstate" \
+    -backend-config="access_key=$2" 
